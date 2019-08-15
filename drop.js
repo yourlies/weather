850ef566ceb727a254a0aa0ce8bbf1bd8a18b899
Lies <958142428@qq.com>
2019-08-15 21:28:29 +0800

    添加了下雨之后爆裂雨滴的效果

---
+++ drop.js
@@ -0,0 +1,69 @@
+;(function () {
+  var getRgb = function (r, g, b) {
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+  var Drop = function (options) {
+    this.options = options || {};
+    this.gravity = this.options.gravity || 0.5;
+    this.canv = this.options.canv;
+    this.ctx = this.options.ctx;
+    this.drops = [];
+  };
+  Drop.prototype.update = function () {
+    var _this =  this;
+    if (this.drops.length > 0) {
+      this.drops.forEach(function (drop) {
+        drop.posx = drop.posx + drop.vx;
+        drop.vy = drop.vy + _this.gravity;
+        drop.posy = drop.posy + drop.vy;
+        drop.color = drop.color + 6;
+        if (drop.posy > _this.canv.clientHeight) {
+          drop.die = true;
+        }
+      });
+    }
+    for (var i = this.drops.length - 1; i >= 0; i--) {
+      if (this.drops[i].die) {
+        this.drops.splice(i, 1);
+      }
+    }
+  }
+  Drop.prototype.render = function () {
+    var _this = this;
+    this.ctx.lineWidth = 2;
+    this.drops.forEach(function (e) {
+      _this.ctx.strokeStyle = getRgb(e.color, e.color, e.color);
+      _this.ctx.beginPath();
+      _this.ctx.arc(e.posx, e.posy, e.radius, 0.2 * Math.PI, 2 * Math.PI);
+      _this.ctx.stroke();
+    });
+  }
+  Drop.prototype.create = function (e) {
+    var entries = Math.floor(Math.random() * 2 + 4);
+    for (var i = 0; i < entries; i++) {
+      var drop = {
+        die: false,
+        posx: e.x, posy: e.y,
+        vx: e.vx || (Math.random() - 0.5) * 8,
+        vy: e.vy || (Math.random() * (-6) - 3),
+        radius: e.radius || Math.random() * 1 + .5,
+        color: e.color || 0
+      };
+      if (e.forward == 1) {
+        drop.vx = e.vyx|| (Math.random() * 8);
+      }
+      this.drops.push(drop);
+    }
+  }
+
+  Drop.prototype.watcher = function (e) {
+    this.create(e);
+  }
+  Drop.prototype.updater = function (e) {
+    this.update();
+    this.render();
+  }
+
+  window.$process = window.$process || {};
+  $process.Drop = Drop;
+})();
--- weather.js
+++ weather.js
@@ -1,1 +1,205 @@
-!function(){var a={};a.scene=document.getElementById("canv"),a.camera=a.scene.getContext("2d"),a.entity=20,a.particles=[],a.time=0,a.width=window.innerWidth,a.height=window.innerHeight,a.entity=Math.floor(a.width*a.height/4e4),a.FPS=60,a.g=7,a.leans=.5;var t={random:function(t,i){return Math.random()*(i-t)+t}};window.requestAnimationFrame=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(t){window.setTimeout(t,1e3/a.FPS)},window.cancelAnimationFrame=window.cancelAnimationFrame||window.clearTimeout;function i(){this.x=Math.random()*a.width,this.y=Math.random()*a.height,this.moveX=this.x,this.moveY=this.y,this.r=t.random(2,4),this.alpha=t.random(.3,1),this.status=!1,this.velocity={x:t.random(-.35,.35),y:t.random(.75,1.5)},this.chance=1}i.prototype.update=function(t){this.updater=t,this.updater(a.camera,this)},i.prototype.render=function(t){"function"==typeof t&&(this.handle=t),a.time+=.016,this.x+=this.velocity.x,this.y+=this.velocity.y,(this.y>a.scene.height||this.x>a.scene.width)&&(Math.random()>this.chance?(this.x=Math.random()*a.scene.width,this.y=0):(this.x=0,this.y=Math.random()*a.scene.height),this.status=!1)};var e,n=function(){if(window.istop)return requestAnimationFrame(n),!1;a.camera.clearRect(0,0,a.scene.width,a.scene.height);for(var t=0;t<a.particles.length;t++){var i=a.particles[t];if("rainy"!=i.status){a.height*a.leans>a.height?i.chance=a.width/a.leans/a.height*.5:i.chance=a.height*a.leans/a.width*.5,i.velocity.y=Math.floor(8+2*Math.random()),i.g=a.g;var e=Math.random();switch(!0){case.66<e:i.increment=40;break;case.33<e:i.increment=35,i.velocity.y=.8*i.velocity.y,i.g=.8*a.g;break;default:i.increment=30,i.velocity.y=.6*i.velocity.y,i.g=.6*a.g}i.velocity.x=i.velocity.y*a.leans,i.alpha=.1,i.status="rainy",i.back=!1,.7<Math.random()&&(i.velocity.y=.5*i.velocity.y,i.g=.5*a.g,i.back=!0)}i.render(),i.update(function(t){t.beginPath(),t.moveTo(this.x,this.y),this.velocity.y+=1/60*this.g,this.velocity.x=this.velocity.y*a.leans;var i=Math.atan(this.velocity.y/this.velocity.x),e=Math.cos(i)*this.increment,n=Math.sin(i)*this.increment;t.lineTo(this.x+e,this.y+n),this.moveX=this.x+e,this.moveY=this.y+n,t.lineWidth=2,t.strokeStyle="rgba(0, 0, 0, "+this.alpha+")",t.stroke(),t.closePath()})}requestAnimationFrame(n)},h=0,o=function(){if(60<=++h){h=0;var t=new i;a.particles.push(t)}a.particles.length>=a.entity?cancelAnimationFrame(e):e=requestAnimationFrame(o)};a.scene.width=a.width,a.scene.height=a.height,o(),n(),window.stop=function(){window.istop=!0},window.begin=function(){window.istop=!1}}();
+(function () {
+  var snowy = function () {
+    Nature.camera.clearRect(0, 0, Nature.scene.width, Nature.scene.height);
+    for (var i = 0; i < Nature.particles.length; i++) {
+      var particle = Nature.particles[i];
+      particle.render();
+      particle.update(function (camera) {
+        camera.fillStyle = 'rgba(255, 255, 255, ' + this.alpha + ')';
+        camera.beginPath();
+        camera.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        camera.closePath();
+        camera.fill();
+      });
+    }
+    requestAnimationFrame(snowy);
+  }
+});
+
+;(function () {
+  var Nature = {};
+  Nature.scene = document.getElementById('canv');
+  Nature.camera = Nature.scene.getContext('2d');
+  Nature.particles = [];
+  Nature.width = window.innerWidth;
+  Nature.height = window.innerHeight;
+  Nature.entity = Math.floor((Nature.width * Nature.height) / 60000);
+  // Nature.entity = 2;
+  Nature.FPS = 60;
+  Nature.g = 5;
+  Nature.leans = 0.5;
+
+  var Tool = {};
+  Tool.random = function (min, max) {
+    return Math.random() * (max - min) + min
+  }
+
+  window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame
+      || window.webkitRequestAnimationFrame
+      || window.mozRequestAnimationFrame
+      || window.oRequestAnimationFrame
+      || window.msRequestAnimationFrame
+      || function (callBack) {
+        window.setTimeout(callBack, 1000 / Nature.FPS)
+      }
+  })();
+
+  window.cancelAnimationFrame = (function () {
+    return window.cancelAnimationFrame || window.clearTimeout;
+  })();
+
+  var getTwoLineDistance = function (lineA, lineB) {
+    var k = (lineA.y0 - lineA.y1) / (lineA.x0 - lineA.x1);
+    var b = lineA.y0 - k * lineA.x0;
+    var d = Math.abs(k * lineB.x0 + -1 * lineB.y0 + b) / Math.sqrt(k * k + 1);
+    var isOverlapped = false;
+    if ((lineA.x0 < lineB.x0) && (lineA.x1 > lineB.x0)) {
+      isOverlapped = true;
+    }
+    if ((lineA.x0 > lineB.x0) && (lineB.x1 > lineA.x0)) {
+      isOverlapped = true;
+    }
+    return { d: d, isOverlapped: isOverlapped };
+  }
+
+  var Particle = function () {
+    this.x = Math.random() * Nature.width;
+    this.y = Math.random() * Nature.height;
+    this.moveX = this.x;
+    this.moveY = this.y;
+    this.r = Tool.random(2, 4);
+    this.alpha = Tool.random(0.3, 1);
+    this.status = false;
+    this.velocity = {
+      x: Tool.random(-0.35, 0.35),
+      y: Tool.random(0.75, 1.5)
+    }
+    this.chance = 1;
+  }
+  Particle.prototype.update = function (updater) {
+    this.updater = updater;
+    this.updater();
+  }
+  Particle.prototype.render = function (recycle) {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    if (this.moveY > Nature.scene.height) {
+      if (typeof recycle == 'function') {
+        this.recycle = recycle;
+        this.recycle(this);
+      }
+    }
+    if ((this.moveY > Nature.scene.height) || (this.x > Nature.scene.width)) {
+      var chance = Math.random();
+      if (chance > this.chance) {
+        this.x = Math.random() * Nature.scene.width;
+        this.y = 0;
+      } else {
+        this.x = 0;
+        this.y = Math.random() * Nature.scene.height;
+      }
+      this.status = false;
+    }
+  }
+
+  var RainyAdapt = function (particle) {
+    if (Nature.height * Nature.leans > Nature.height) {
+      particle.chance = ((Nature.width / Nature.leans) / Nature.height) * 0.5;
+    } else {
+      particle.chance = ((Nature.height * Nature.leans) / Nature.width) * 0.5;
+    }
+    particle.velocity.y = Math.floor(20 + Math.random() * 5);
+    particle.g = Nature.g;
+    particle.velocity.x = particle.velocity.y * Nature.leans;
+    particle.alpha = 0.1;
+    particle.status = 'rainy';
+    particle.back = false;
+  }
+  var RainyLayer = function (particle) {
+    var rate = Math.random();
+    switch (true) {
+      case rate > 0.66:
+        particle.increment = 55;
+        break;
+      case rate > 0.33:
+        particle.increment = 50;
+        particle.velocity.y = particle.velocity.y * 0.8;
+        particle.g = Nature.g * 0.8;
+        break;
+      default:
+        particle.increment = 40;
+        particle.velocity.y = particle.velocity.y * 0.6;
+        particle.g = Nature.g * 0.6;
+        particle.alpha = particle.alpha * 0.6;
+        break;
+    }
+  }
+
+  var RainyUpdater = function (ctx) {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    this.velocity.y += (1 / 60) * this.g;
+    this.velocity.x = this.velocity.y * Nature.leans;
+    var t = Math.atan(this.velocity.y / this.velocity.x);
+    var x = Math.cos(t) * this.increment;
+    var y = Math.sin(t) * this.increment;
+    ctx.lineTo(this.x + x, this.y + y);
+
+    this.moveX = this.x + x;
+    this.moveY = this.y + y;
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = 'rgba(0, 0, 0, ' + this.alpha + ')';
+    ctx.stroke();
+    ctx.closePath();
+  };
+
+  var Rainy = function (options) {
+    this.ctx = options.ctx;
+    this.recycle = options.recycle;
+  };
+  Rainy.prototype.updater = function () {
+    var _this = this;
+    for (var i = 0; i < Nature.particles.length; i++) {
+      var particle = Nature.particles[i];
+      if (particle.status != 'rainy') {
+        RainyAdapt(particle);
+        RainyLayer(particle);
+      }
+      particle.render(this.recycle);
+      particle.update(function () {
+        this.RainyUpdater = RainyUpdater;
+        this.RainyUpdater(_this.ctx);
+      });
+    }
+  }
+
+  var weatherId;
+  var frame = 0;
+  var weather = function () {
+    frame++;
+    if (frame >= 60) {
+      frame = 0;
+      var particle = new Particle();
+      Nature.particles.push(particle);
+    }
+    if (Nature.particles.length >= Nature.entity) {
+      cancelAnimationFrame(weatherId);
+    } else {
+      weatherId = requestAnimationFrame(weather);
+    }
+  }
+  weather();
+
+  Nature.scene.width = Nature.width;
+  Nature.scene.height = Nature.height;
+
+  window.$process = window.$process || {};
+  $process.Rainy = Rainy;
+  // var rainy = new Rainy();
+  // var watcher = function () {
+  //   rainy.watcher();
+  //   requestAnimationFrame(watcher);
+  // };
+  // requestAnimationFrame(watcher);
+})();
