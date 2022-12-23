4589e5ee3cc9229ba55d5935654593415debea9c
因雨而生 <958142428@qq.com>
2022-12-23 17:19:29 +0800

    save

--- dev.js
+++ dev.js
@@ -1,12 +1,6 @@
 var exec = require("child_process").exec;
 function execute(cmd) {
-  exec(cmd, function (error, stdout, stderr) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log("success");
-    }
-  });
+  exec(cmd);
 }
 
 setInterval(() => {
